ab3d9e33c7dec80fdb3e0b83ce91495b8d270681
geracnubiral <gerardo.collante@nubiral.com>
2024-09-29 14:08:04 -0300

    Replace dependency on deprecated pkg_resources
    Fixes #9

--- iso18245/__init__.py
+++ iso18245/__init__.py
@@ -1,8 +1,6 @@
 import csv
-import os.path
-from typing import Any, Dict, List, NamedTuple
-
-from pkg_resources import resource_filename
+from importlib.resources import files
+from typing import Dict, List, NamedTuple
 
 ISO_VERSION_YEAR = 2003
 
@@ -42,8 +40,8 @@ _cached_csv: Dict[str, List[List[str]]] = {}
 
 def _load_csv(path: str) -> List[List[str]]:
 	if path not in _cached_csv:
-		full_path = resource_filename("iso18245", os.path.join("data", path))
-		with open(full_path, "r") as f:
+		ref = files("iso18245") / "data" / path
+		with ref.open("r") as f:
 			reader = csv.reader(f)
 			_cached_csv[path] = list(reader)[1:]
 
